3b37158fa7754daf1fed025df995502e002c4845
hyperbayse <hyperbayse@gmail.com>
2024-08-21 15:04:57 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,11 @@ def number_of_subscribers(subreddit):
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    .format(subreddit),
+    headers={"User-Agent": "My-User-Agent"},
+    allow_redirects=False)
+    
     if sub_info.status_code >= 300:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+        return sub_info.json().get("data").get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,11 +8,12 @@ def top_ten(subreddit):
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    .format(subreddit),
+    headers={"User-Agent": "My-User-Agent"},
+    allow_redirects=False)
     if sub_info.status_code >= 300:
         print('None')
+        
     else:
         [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        for child in sub_info.json().get("data").get("children")]
